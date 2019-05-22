ef66c7cc7925f79a6ff364a3bbf333abe0ea5f0b
zwillerzoog <marsh.heidi.c@gmail.com>
2019-05-22 14:13:57 -0400

    Updates

---
+++ precache-manifest.6be7f5245fe54b76d090671accaef178.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f0a6e3d3d568f44455adfe301e0fcf0",
+    "url": "/card-game/index.html"
+  },
+  {
+    "revision": "6820b59b1ed97456244a",
+    "url": "/card-game/static/css/main.9c72b2dc.chunk.css"
+  },
+  {
+    "revision": "67cf0129f8b3d8901ad5",
+    "url": "/card-game/static/js/2.b2760ed8.chunk.js"
+  },
+  {
+    "revision": "6820b59b1ed97456244a",
+    "url": "/card-game/static/js/main.2525860b.chunk.js"
+  },
+  {
+    "revision": "ebc24405c5239c99a5a5",
+    "url": "/card-game/static/js/runtime~main.d44b1061.js"
+  }
+]);
